6d23a6667749fccfc081a5f56a8bf449480291cc
NguyenDinhNhatNguyen <ndnnforwork@gmail.com>
2026-02-01 09:20:58 +0700

    Delete Đồ án CE118.Q17 directory

--- Đồ án CE118.Q17/single_cycle/simulation/qsim/work/@d@i@v_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity DIV_vlg_sample_tst is
-    port(
-        A               : in     vl_logic_vector(15 downto 0);
-        B               : in     vl_logic_vector(15 downto 0);
-        sampler_tx      : out    vl_logic
-    );
-end DIV_vlg_sample_tst;
--- Đồ án CE118.Q17/single_cycle/simulation/qsim/work/@d@i@v_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity DIV_vlg_vec_tst is
-end DIV_vlg_vec_tst;
